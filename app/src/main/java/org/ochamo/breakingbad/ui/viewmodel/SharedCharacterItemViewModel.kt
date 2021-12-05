25b386d22856c347eb8a90f96f6a6044f37c9181
Otto Chamo <ochamo@develsystems.com>
2021-12-05 02:13:13 -0600

    feature: Create SharedViewModel between my fragments

---
+++ app/src/main/java/org/ochamo/breakingbad/ui/viewmodel/SharedCharacterItemViewModel.kt
@@ -0,0 +1,38 @@
+package org.ochamo.breakingbad.ui.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import org.ochamo.breakingbad.ui.model.BreakingBadCharacterModel
+import org.ochamo.breakingbad.util.Event
+import javax.inject.Inject
+
+@HiltViewModel
+class SharedCharacterItemViewModel @Inject constructor(): ViewModel() {
+
+    private val _breakingBadItem = MutableLiveData<BreakingBadCharacterModel>()
+
+    val breakingBadItem: LiveData<BreakingBadCharacterModel> = _breakingBadItem
+
+    private val _markAsFavorite = MutableLiveData<Event<BreakingBadCharacterModel>>()
+
+    val markAsFavorite: LiveData<Event<BreakingBadCharacterModel>> = _markAsFavorite
+
+
+    fun markOrUnMarkAsFavorite(selectedModel: BreakingBadCharacterModel) {
+        if (selectedModel.isFavorite.get()) {
+            selectedModel.isFavorite.set(false)
+        } else {
+            selectedModel.isFavorite.set(true)
+        }
+        _markAsFavorite.value = Event(selectedModel)
+    }
+
+
+    fun setItem(selected: BreakingBadCharacterModel) {
+        _breakingBadItem.value = selected
+    }
+
+
+}
